cd20d572b821e7ddacecb0277e2022523e3f7b0c
James Cacioppo <james.m.cacioppo@gmail.com>
2022-12-05 17:56:05 +0000

    Switch statement to determine who won

--- puz2.py
+++ puz2.py
@@ -1,4 +1,4 @@
-opponentPlay = {
+opponent_play = {
     "a": "rock",
     "b": "paper",
     "c": "scissors"
@@ -31,3 +31,21 @@ for i in f.readlines():
 
 f.close()
 
+for i in lines:
+    round = i.split()
+    print("0 is", round[0])
+    print("1 is", round[1])
+
+    # Determine win/loss for a
+    if round[0] == "a": # Rock
+        print("Opponent played rock")
+
+        match round[1]:
+            case "X": # Rock
+                result = "draw"
+            case "Y": # Paper
+                result = "win"
+            case "Z": # Scissors
+                result = "loss"
+    elif round[0] == "B": # Paper
+        print("Opponent played paper")
